85dde24dbb15c324b118656f2f56997089333a4e
Matjaž Horvat <matjaz.horvat@gmail.com>
2018-11-27 17:30:04 +0100

    Load comments from DB

--- pontoon/base/views.py
+++ pontoon/base/views.py
@@ -36,6 +36,7 @@ from six.moves.urllib.parse import urlparse
 from pontoon.base import forms
 from pontoon.base import utils
 from pontoon.base.models import (
+    Comment,
     Entity,
     Locale,
     Project,
@@ -393,6 +394,23 @@ def get_translation_history(request):
         payload.append(translation_dict)
 
     return JsonResponse(payload, safe=False)
+
+
+@utils.require_AJAX
+def get_comments(request):
+    """Get comments for the given entity and locale."""
+    try:
+        entity = request.GET['entity']
+        locale = request.GET['locale']
+    except MultiValueDictKeyError as e:
+        return HttpResponseBadRequest('Bad Request: {error}'.format(error=e))
+
+    entity = get_object_or_404(Entity, pk=entity)
+    locale = get_object_or_404(Locale, code=locale)
+
+    comments = Comment.objects.filter(entity=entity)
+
+    return JsonResponse(comments.serialize(), safe=False)
 
 
 @utils.require_AJAX
